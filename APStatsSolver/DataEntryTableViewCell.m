751aa770d60bc3fd49eecbee7bf1c3727a04b2d8
Skyler <skyarnold1@me.com>
2015-09-27 16:00:54 -0600

    god knows all the things which changed since last time, but I need to move this to david's computer

---
+++ APStatsSolver/DataEntryTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DataEntryTableViewCell.m
+//  APStatsSolver
+//
+//  Created by Skyler Arnold on 9/26/15.
+//  Copyright © 2015 Skyler Arnold. All rights reserved.
+//
+
+#import "DataEntryTableViewCell.h"
+
+@implementation DataEntryTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ APStatsSolver/ManualDataEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ManualDataEntryViewController.h
+//  APStatsSolver
+//
+//  Created by Skyler Arnold on 9/26/15.
+//  Copyright © 2015 Skyler Arnold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManualDataEntryViewController : UIViewController
+
+@end
